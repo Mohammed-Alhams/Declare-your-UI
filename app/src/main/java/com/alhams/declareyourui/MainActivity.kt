cc18ac314bdbb843edaf4d88fbe8d28e0636fec0
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-11-11 20:35:19 +0200

    Add Default theme

--- app/src/main/java/com/alhams/declareyourui/MainActivity.kt
+++ app/src/main/java/com/alhams/declareyourui/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Modifier
 import com.alhams.declareyourui.ui.components.App
+import com.alhams.declareyourui.ui.theme.DeclareYourUITheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App(Modifier.fillMaxWidth())
+            DeclareYourUITheme {
+                App(Modifier.fillMaxWidth())
+            }
         }
     }
 }
--- app/src/main/java/com/alhams/declareyourui/ui/components/onboarding_screen.kt
+++ app/src/main/java/com/alhams/declareyourui/ui/components/onboarding_screen.kt
@@ -2,7 +2,7 @@ package com.alhams.declareyourui.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -20,7 +20,7 @@ fun OnboardingScreen(
 
     Column(
         modifier = modifier
-            .fillMaxWidth(),
+            .fillMaxHeight(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
