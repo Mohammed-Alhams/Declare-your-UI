3c94187ac2f87cd010b880af05adafc224ba8b10
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-11-11 20:35:33 +0200

    Persisting state

--- app/src/main/java/com/alhams/declareyourui/ui/components/app.kt
+++ app/src/main/java/com/alhams/declareyourui/ui/components/app.kt
@@ -5,7 +5,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,7 +14,7 @@ import com.alhams.declareyourui.ui.theme.DeclareYourUITheme
 @Composable
 fun App(modifier: Modifier = Modifier) {
 
-    var shouldShowOnboardingScreen by remember {
+    var shouldShowOnboardingScreen by rememberSaveable {
         mutableStateOf(true)
     }
 
