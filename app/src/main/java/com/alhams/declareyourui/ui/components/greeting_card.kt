bf33e59f09f7cd5d5a078cb7ee0c9b1bf4b7520c
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-11-11 19:18:13 +0200

    States in compose

--- app/src/main/java/com/alhams/declareyourui/ui/components/greeting_card.kt
+++ app/src/main/java/com/alhams/declareyourui/ui/components/greeting_card.kt
@@ -8,12 +8,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun Greeting(name: String) {
+
+    var expanded by remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -22,13 +31,16 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
 
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            ElevatedButton(onClick = {
+                expanded = !expanded
+            }) {
+                Text(text = if (expanded) "Show less" else "Show more")
             }
         }
     }
