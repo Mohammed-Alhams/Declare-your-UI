362a030800d14af2774db59b258b5de0879f974b
Mohammed Alhams <mohammed.alhamsma28@engineer.com>
2022-11-15 21:56:13 +0200

    Home section - Slot APIs

--- app/src/main/java/com/alhams/declareyourui/ui/theme/Type.kt
+++ app/src/main/java/com/alhams/declareyourui/ui/theme/Type.kt
@@ -2,9 +2,35 @@ package com.alhams.declareyourui.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.alhams.declareyourui.R
+
+private val fontFamilyKulim = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.kulim_park_regular
+        ),
+        Font(
+            resId = R.font.kulim_park_light,
+            weight = FontWeight.Light
+        )
+    )
+)
+
+private val fontFamilyLato = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.lato_regular
+        ),
+        Font(
+            resId = R.font.lato_bold,
+            weight = FontWeight.Bold
+        )
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,7 +40,8 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +58,35 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+    headlineLarge = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontWeight = FontWeight.Light,
+        fontSize = 28.sp,
+        letterSpacing = (1.15).sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontSize = 15.sp,
+        letterSpacing = (1.15).sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    bodySmall = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = (1.15).sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontSize = 12.sp,
+        letterSpacing = (1.15).sp
+    ),
 )
